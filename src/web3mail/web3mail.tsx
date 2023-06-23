1bfcf24f309f4ddd0a077277c099e1f9c312f638
Le-Caignec <robinlecaignec@yahoo.fr>
2023-06-23 16:02:08 +0200

    resolve bug

--- src/web3mail/web3mail.tsx
+++ src/web3mail/web3mail.tsx
@@ -1,27 +1,35 @@
-import { IExecWeb3Mail } from '@iexec/web3mail';
+import { IExecWeb3mail } from '@iexec/web3mail';
 import { getAccount } from '@wagmi/core';
 
 //fetch my contacts by calling fetchMyContacts method from @iexec/web3mail
 export const fetchMyContacts = async () => {
   try {
-  const account = getAccount();
-  const provider = await account.connector?.getProvider();
-  const web3mail = new IExecWeb3Mail(provider);
+    const account = getAccount();
+    const provider = await account.connector?.getProvider();
+    const web3mail = new IExecWeb3mail(provider);
     const contacts = await web3mail.fetchMyContacts();
-  return contacts;
-    } catch (error) {
+    return contacts;
+  } catch (error) {
     console.error('Error fetching contacts:', error);
     throw error;
   }
 };
-export const sendMail = async (mailObject: string, mailContent: string, protectedData: string ) => {
+export const sendMail = async (
+  emailSubject: string,
+  emailContent: string,
+  protectedData: string
+) => {
   try {
-  const account = getAccount();
-  const provider = await account.connector?.getProvider();
-  const web3mail = new IExecWeb3Mail(provider);
-    const txHash = await web3mail.sendEmail({mailObject ,mailContent,protectedData });
-  return txHash;
-} catch (error) {
+    const account = getAccount();
+    const provider = await account.connector?.getProvider();
+    const web3mail = new IExecWeb3mail(provider);
+    const txHash = await web3mail.sendEmail({
+      emailSubject,
+      emailContent,
+      protectedData,
+    });
+    return txHash;
+  } catch (error) {
     console.error('Error sending web3 mail:', error);
     throw error;
   }
